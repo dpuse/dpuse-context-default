29bb3b30c12d9824d2ffcfdf0df21ade93b8815a
Jonathan Terrell <terrell.jm@gmail.com>
2025-09-03 20:12:19 +0200

    v0.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 // Dependencies - Framework.
-import type { Context, ContextConfig } from '@datapos/datapos-share-core';
-
-// Dependencies - Data.
+import type { Context, ContextConfig, ContextFocusConfigListResult, ContextFocusConfigListSettings } from '@datapos/datapos-shared';
 import config from '../config.json';
-import data from './data.json';
 
-// Constants
-
-// Classes - File Store Emulator Connector
-export default class FileStoreEmulatorConnector implements Context {
+// Classes - Default context.
+export default class DefaultContext implements Context {
     readonly config: ContextConfig;
 
     constructor() {
         this.config = config as ContextConfig;
+    }
+
+    // Operations - List focuses.
+    async listFocuses(settings?: ContextFocusConfigListSettings): Promise<ContextFocusConfigListResult> {
+        const focusConfigs = config.focuses;
+        return { focusConfigs };
     }
 }

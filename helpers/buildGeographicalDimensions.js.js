b7de1ad96798ebc3ae2049e586214d5a736e5413
Jonathan Terrell <terrell.jm@gmail.com>
2025-09-07 21:24:14 +0200

    v0.0.28

---
+++ helpers/buildGeographicalDimensions.js.js
@@ -0,0 +1,5 @@
+import fs from 'fs/promises';
+
+async function buildGeographicalDimensions() {}
+
+buildGeographicalDimensions();
--- helpers/retrieveCountryData.js
+++ helpers/retrieveCountryData.js
@@ -1,13 +1,13 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 
 async function retrieveCountryData() {
     const responseForIndependent = await fetch('https://restcountries.com/v3.1/independent?status=true');
     const dataForIndependent = await responseForIndependent.text();
-    fs.writeFileSync('./helpers/data/retrievals/countryDataFromCountryRestIndependent.json', dataForIndependent);
+    fs.writeFile('./helpers/data/retrievals/countryDataFromCountryRestIndependent.json', dataForIndependent, 'utf-8');
 
     const responseForDependent = await fetch('https://restcountries.com/v3.1/independent?status=false');
     const dataForDependent = await responseForDependent.text();
-    fs.writeFileSync('./helpers/data/retrievals/countryDataFromCountryRestDependent.json', dataForDependent);
+    fs.writeFile('./helpers/data/retrievals/countryDataFromCountryRestDependent.json', dataForDependent, 'utf-8');
 }
 
 retrieveCountryData();

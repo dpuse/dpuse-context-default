99b62d68ce75615b0712d2a223736907791c2663
Jonathan Terrell <terrell.jm@gmail.com>
2025-09-09 12:30:16 +0200

    v0.0.34

--- helpers/transformGeographicalData.js
+++ helpers/transformGeographicalData.js
@@ -15,35 +15,42 @@ async function transformCountryData() {
     console.log('Country count (Dependent)__:', countriesDependent.length);
     console.log('Country count______________:', countries.length);
     // console.log('\nFirst country______________:', countries[0]);
+    console.log('\n');
+
+    const nationalities = {};
+    const translations = {};
 
     const geoCountries = [];
     // const currencies = {};
     for (const country of countries) {
         const label = { en: country.name.common };
+        const sortedLabel = Object.fromEntries(Object.entries(label).sort(([leftKey], [rightKey]) => leftKey.localeCompare(rightKey)));
+
         const labelOfficial = { en: country.name.official };
-        for (const [key, translation] of Object.entries(country.translations)) {
-            const alpha2 = lookupLocaleCode(key);
-            if (!alpha2) continue;
-            label[alpha2] = translation.common;
-            labelOfficial[alpha2] = translation.official;
-        }
-        const sortedLabel = Object.fromEntries(Object.entries(label).sort(([keyA], [keyB]) => keyA.localeCompare(keyB)));
-        const sortedLabelOfficial = Object.fromEntries(Object.entries(labelOfficial).sort(([keyA], [keyB]) => keyA.localeCompare(keyB)));
+        const sortedLabelOfficial = Object.fromEntries(Object.entries(labelOfficial).sort(([leftKey], [rightKey]) => leftKey.localeCompare(rightKey)));
 
         const geoRegion = geoRegions.find((geoRegion) => geoRegion.label.en === country.region);
         const geoSubregion = geoSubregions.find((geoSubregion) => geoSubregion.label.en === country.subregion);
 
         // for (const [key, currency] of Object.entries(country.currencies || {})) currencies[key] = (currencies[key] || 0) + 1;
 
+        for (const value of Object.values(country.demonyms.eng || {})) {
+            if (value) nationalities[country.cca2] = { label: { en: value } };
+        }
+
+        for (const key of Object.keys(country.translations || {})) translations[key] = { count: (translations[key]?.count || 0) + 1 };
         for (const [key, translation] of Object.entries(country.translations)) {
-            const localeCode = lookupLocaleCode(key);
-            if (!localeCode) continue;
-            label[localeCode] = translation.common;
-            labelOfficial[localeCode] = translation.official;
+            const locale = lookupLanguageUsingAlpha3(key);
+            if (!locale) continue;
+            label[locale.alpha2] = translation.common;
+            labelOfficial[locale.alpha2] = translation.official;
         }
 
         geoCountries.push({
             id: String(country.cca2).toLocaleLowerCase('en'),
+            id3: String(country.cca3).toLocaleLowerCase('en'),
+            idCIOC: String(country.cioc).toLocaleLowerCase('en'),
+            idNum: String(country.ccn3),
             label: sortedLabel,
             labelOfficial: sortedLabelOfficial,
             capitals: country.capital,
@@ -53,24 +60,42 @@ async function transformCountryData() {
             regionId: geoRegion.id,
             subregionId: geoSubregion?.id
         });
-        if (country.capital?.length > 1) console.log('MULTIPLE CAPITALS__:', country.name.common, ',', country.capital);
-        if (country.continents.length > 1) console.log('MULTIPLE CONTINENTS:', country.name.common, ',', country.continents);
-        if (Object.keys(country.currencies ?? {}).length > 1) console.log('MULTIPLE CURRENCIES:', country.name.common, ',', country.currencies);
+
+        if (country.capital?.length > 1) console.log('! Multiple capitals________:', country.name.common, '-', country.capital);
+        if (country.continents.length > 1) console.log('! Multiple continents______:', country.name.common, '-', country.continents);
+        if (Object.keys(country.currencies ?? {}).length > 1) console.log('! Multiple currencies______:', country.name.common);
     }
+
     await fs.writeFile('./helpers/data/geoCountries.json', JSON.stringify(geoCountries, null, 4), 'utf-8');
 
-    // const sortedCurrencies = Object.fromEntries(Object.entries(currencies).sort(([keyA], [keyB]) => keyA.localeCompare(keyB)));
+    // const sortedCurrencies = Object.fromEntries(Object.entries(currencies).sort(([leftKey], [rightKey]) => leftKey.localeCompare(rightKey)));
     // console.log('CURRENCIES_________:', sortedCurrencies);
+
+    console.log('\nNationalities______________:', nationalities);
+
+    for (const [key, value] of Object.entries(translations || {})) {
+        value.id2 = lookupLanguageUsingAlpha3(key)?.alpha2 || undefined;
+        value.id3B = lookupLanguageUsingAlpha3(key)?.['alpha3-b'] || undefined;
+        value.id3T = lookupLanguageUsingAlpha3(key)?.['alpha3-t'] || undefined;
+        value.label = { en: lookupLanguageUsingAlpha3(key)?.English || undefined };
+    }
+    // const sortedTranslations = Object.fromEntries(Object.entries(translations).sort(([leftKey], [rightKey]) => leftKey.localeCompare(rightKey)));
+    // console.log('\nTranslations_______________:', sortedTranslations);
+    const geoLanguages = [];
+    for (const language of languages) {
+        geoLanguages.push({ id: language['alpha3-b'], idT: language['alpha3-t'] || undefined, id2: language.alpha2 || undefined, label: { en: language.English } });
+    }
+    await fs.writeFile('./helpers/data/geoLanguages.json', JSON.stringify(geoLanguages, null, 4), 'utf-8');
 }
 
-function lookupLocaleCode(key) {
-    const alpha3BMatch = languages.find((item) => item['alpha3-b'] === key);
-    if (alpha3BMatch) return alpha3BMatch.alpha2;
+function lookupLanguageUsingAlpha3(alpha3Code) {
+    const alpha3BMatch = languages.find((item) => item['alpha3-b'] === alpha3Code);
+    if (alpha3BMatch) return alpha3BMatch;
 
-    const alpha3TMatch = languages.find((item) => item['alpha3-t'] === key);
-    if (alpha3TMatch) return alpha3TMatch.alpha2;
+    const alpha3TMatch = languages.find((item) => item['alpha3-t'] === alpha3Code);
+    if (alpha3TMatch) return alpha3TMatch;
 
-    console.log('MISSING LOCALE_____:', key);
+    console.log('! Missing Locale___________:', alpha3Code);
     return undefined;
 }
 

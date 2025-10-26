f0d4aebf72c07bf8b76c40e6b8312ac03ec4e221
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-10-26 14:03:14 +0100

    v0.0.81

---
+++ eslint.config.ts
@@ -0,0 +1,25 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+    {
+        files: ['vite.config.ts', 'src/*.ts'],
+        languageOptions: { parser: tseslintParser, parserOptions: { project: './tsconfig.json' } },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            import: importPlugin
+        },
+        rules: {
+            '@typescript-eslint/consistent-type-imports': 'warn',
+            '@typescript-eslint/no-import-type-side-effects': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
+
+            'import/no-duplicates': 'off',
+            'sort-imports': ['warn', { allowSeparatedGroups: true, ignoreCase: true, memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple'] }],
+
+            'no-empty': 'warn',
+            'prefer-const': 'warn'
+        }
+    }
+];
